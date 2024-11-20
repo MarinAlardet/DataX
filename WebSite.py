8c4e4febc650a10b66f619d2604b03b90a0210dd
MarinAlardet <131903480+MarinAlardet@users.noreply.github.com>
2024-11-19 19:51:22 -0600

    Update WebSite.py

--- WebSite.py
+++ WebSite.py
@@ -34,7 +34,7 @@ def render_header():
 
     col1, col2 = st.columns([1, 3])
     with col1:
-        st.image("pictures/ternium_logo.png", width=100)
+        st.image("pictures/ternium_logo.png", width=300)
     with col2:
         st.markdown(
             """
